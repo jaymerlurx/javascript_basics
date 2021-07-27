43fdb16b2c16c3748e8094b0a045e3c1c4f51c6c
jaymerlurx <jordan_wiggs@yahoo.com>
2021-07-27 16:21:55 -0500

    initial commit

---
+++ javascript_basics.js
@@ -0,0 +1,136 @@
+//We codin
+
+//console.log('Hello World!');
+
+//console.log(Date());
+
+//console.log("Hello" + " " + "World");
+//console.log(`Hello World`);
+//
+//*************************************
+
+// var name = 'Jordan';
+// console.log(name);
+
+// let age = 25;
+// console.log(age);
+
+// const isRobot = false;
+//console.log(isRobot);
+//console.log("My name is" + " " + name " " + "I am" + " " + age + " " + "years old!");
+
+//console.log(`My name is: ${name}. I am ${age} years old!`);
+
+//console.log(2 + 6);
+
+//*******************************************************
+
+// const justiceLeague = ['Superman', 'Batman', 'Wonder Woman'];
+// console.log(justiceLeague);
+
+// console.log(justiceLeague[0]);
+// console.log(justiceLeague[1]);
+
+// console.log(justiceLeague.includes('Flash'));
+
+// const person  = {
+//     name: 'Jordan',
+//     age: 3,
+//     isRobot: false,
+// };
+
+// console.log(person.name);
+
+// const { name, age, isRobot } = person;
+// console.log(name, age, isRobot);
+
+// if ( age >= 21 ) {
+//     console.log("Getting WAsted!!");
+// } else {
+//     console.log('nahhh');
+// }
+
+//**************************************************
+//while loop: iterates block of javascript given while condition is true
+
+// let number = 0;
+
+// while ( number <= 10 ) {
+//     console.log (number);
+//     number = number + 1;
+// }
+
+// //for loop: similar to while loops, except inside of while considition is true, loop through a block of code a number of times
+// //number of loops or iterations are based on variable set inside for loop
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(`i is currently: ${i}`);
+// }
+
+
+
+
+//for every number in 100
+//fizzbuzz challenge
+// if number is divisible by 3 print fizz
+// if number is divisible by 5 print buzz
+// if number is divisible by 3 + 5 print fizzbuzz
+// if number is not divisible by 3 or 5 print the number
+
+// for(let i = 0; i <= 100; i++) {
+//     if (i % 3 === 0) {
+//         console.log(`Fizz: ${i}`);
+//     } 
+//     if (i % 5 === 0) {
+//         console.log(`Buzz: ${i}`);
+//     }
+//     if (i % 3=== 0 && i % 5 === 0) {
+//         console.log(`FizzBuzz: ${i}`);
+
+//}
+//**********************************************************
+
+// function oldAddNumbers(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(oldAddNumbers(2, 3));
+
+// const newAddNumbers = (num1, num2) => {
+//     return num1 + num2;
+// };
+
+// console.log(newAddNumbers(2, 3));
+
+// JavaScript Basics Test #1
+// In this test you will be putting together everything you have learned thus far
+// You will create a function ( either Es6 or pre-Es6 ) and the function will take in one parameter
+// The parameter is going to be an array, you can set this as a default using Es6 syntax
+// alternatively if you want to use pre-Es6 here is the array to pass when you call the function
+// const numberArray = [2, 5, 8, 3, 11, 4, 15]
+// 1. Create a function that takes an array as a parameter
+// 2. Loop through the array
+// 3. While looping through the array grab all numbers that are greater than 5
+// 4. Find the sum of all the numbers that are greater than 5
+// to find the length of an array you do array.length
+
+// const testFunction = (array = [2, 5, 8, 3, 11, 4, 15]) => {
+//     let sum = 0;
+    
+//     for (let i = 0; i < array.length; i++) {
+//   //      console.log(array[i]);
+//         if (array[i] > 5) {
+//             sum = sum + array[i]
+//         }
+    
+//     }
+//     return sum
+
+// };
+
+//     console.log(testFunction([55, 25, 66, 2, 4, 777]));
+
+// //for (let i = 0; ; i++) {}
+
+//**************************************************************************** */
+
